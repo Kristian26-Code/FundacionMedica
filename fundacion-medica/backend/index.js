b507ca77689bec106ddeb66a9721d901a8847f8a
Kristian26-Code <Kristian26-Code@users.noreply.github.com>
2026-04-13 21:09:53 -0500

    fix cors abierto

--- fundacion-medica/backend/index.js
+++ fundacion-medica/backend/index.js
@@ -5,9 +5,9 @@ require('dotenv').config()
 
 const app = express()
 
-// Permite recibir JSON y peticiones desde React
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:3000' }))
+app.use(cors())
 app.use(express.json())
+
 const authRoutes = require('./routes/auth')
 app.use('/api/auth', authRoutes)
 const pacientesRoutes = require('./routes/pacientes')
